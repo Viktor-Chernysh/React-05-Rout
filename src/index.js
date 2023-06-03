02ca5b34b39e911a752ff1b510bc226eba03f58c
Viktor Chernysh <79265847+Viktor-Chernysh@users.noreply.github.com>
2023-06-03 18:45:09 +0300

    change @name@ package.json

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="React-05-Rout">
+    <BrowserRouter basename="react-05-rout">
       <App />
     </BrowserRouter>
   </React.StrictMode>
